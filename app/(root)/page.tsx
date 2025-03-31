c71951ce1cc51a827215205b3e0f8583387eb332
igbanibo-Dengi <igbanibodengi@gmail.com>
2025-03-31 10:46:01 +0100

    Set up database schema and migrations for user and book management

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 import Image from "next/image";
 
-export default function Home() {
+const Home = async () => {
+
+  const result = await db.select().from(users);
+
+  console.log(JSON.stringify(result, null, 2));
+
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-10 xl:p-24">
       Hello World
     </div>
   );
 }
+
+export default Home;
